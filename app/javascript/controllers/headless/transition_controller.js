c89250411df08215a160d7c121595b26046d4527
Tonksthebear <jasonconig@me.com>
2025-03-30 22:23:25 -0700

    Dropdown Progress

---
+++ app/javascript/controllers/headless/transition_controller.js
@@ -0,0 +1,38 @@
+import ApplicationController from "controllers/headless/application_controller"
+import { enterTransition, leaveTransition, toggleTransition } from "headless/el-transition"
+
+export default class extends ApplicationController {
+  static targets = ['listener']
+  static values = {
+    transitioned: Boolean,
+    startAnimation: Boolean
+  }
+
+  connect() {
+    this.startAnimationValue && this.enter()
+  }
+
+  toggle() {
+    this.transitionedValue = !this.transitionedValue
+    this.#transition(this.transitionedValue ? 'enter' : 'leave')
+  }
+
+  enter() {
+    this.transitionedValue = true
+    this.#transition('enter')
+  }
+
+  leave() {
+    this.transitionedValue = false
+    this.#transition('leave')
+  }
+
+  #transition(direction) {
+    this.listenerTargets.forEach((listener) => {
+      eval(`${direction}Transition(listener)`)
+        .then(() => this.element.dispatchEvent(new Event(`transition:${direction}`)))
+    })
+
+    this.transitionedValue ? this.dispatch("enter") : this.dispatch("leave")
+  }
+}
--- config/importmap.rb
+++ config/importmap.rb
@@ -1,3 +1,4 @@
 pin "headless/application", to: "headless/application.js"
+pin "headless/el-transition", to: "libraries/headless/el-transition.js"
 pin_all_from File.expand_path("../app/javascript/controllers/headless", __dir__), under: "controllers/headless"
 pin_all_from File.expand_path("../app/javascript/libraries", __dir__), under: "libraries"
