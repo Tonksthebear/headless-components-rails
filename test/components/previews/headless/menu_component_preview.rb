e086bf04c06304bb0a75ea3aa365718e8725b4f2
Tonksthebear <jasonconig@me.com>
2025-03-31 10:12:09 -0700

    Menu improvements

--- test/components/previews/headless/menu_component_preview.rb
+++ test/components/previews/headless/menu_component_preview.rb
@@ -1,6 +1,6 @@
 # test/components/previews/button_component_preview.rb
 class Headless::MenuComponentPreview < ViewComponent::Preview
   def standard
-    render_with_template(template: "headless/manu_component_preview/standard")
+    render_with_template(template: "headless/menu_component_preview/standard")
   end
 end
