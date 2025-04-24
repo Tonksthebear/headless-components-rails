596dc942fbec69a6fbc56f2dea903a53595dd517
Tonksthebear <jasonconig@me.com>
2025-04-23 18:58:09 -0700

    Update menu to allow raw anchor

--- app/components/headless/menu_component.rb
+++ app/components/headless/menu_component.rb
@@ -6,7 +6,7 @@ module Headless
       Headless::Menu::ItemsComponent.new(items_id: @menu_id, **options)
     end
 
-    def initialize(open: false, disabled: false, anchor: {}, portal: false, **options)
+    def initialize(open: false, disabled: false, anchor: "", portal: false, **options)
       options[:id] = options[:id] || "menu-#{object_id}"
       @menu_id = options[:id]
       @open = open
@@ -23,7 +23,7 @@ module Headless
           controller: "headless--transition headless--menu",
           headless__menu_open_at_start_value: @open,
           headless__menu_portal_value: @portal,
-          headless__menu_anchor_to_value: @anchor[:to],
+          headless__menu_anchor_to_value: @anchor,
           headless__menu_headless__portal_outlet: "[data-headless-portal-id='#{@menu_id}']",
           headless__menu_headless__transition_outlet: "##{@menu_id}",
           headless__transition_headless__portal_outlet: "[data-headless-portal-id='#{@menu_id}']",
--- lib/headless/version.rb
+++ lib/headless/version.rb
@@ -1,3 +1,3 @@
 module Headless
-  VERSION = "0.5.0"
+  VERSION = "0.5.1"
 end
